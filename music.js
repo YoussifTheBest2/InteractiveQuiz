603148c0ea17e83189c4706311d778441383dc75
YoussifTheBest2 <youssiftameradam@gmail.com>
2025-01-21 03:12:14 +0200

    added some stuff 👍

--- music.js
+++ music.js
@@ -1,8 +1,21 @@
 const audioSrc = "./Imgs/audio.mp3";
-const volume = 0.4;
+const volume = 0.2;
 const loop = true;
 
 const backgroundMusic = new Audio(audioSrc);
 backgroundMusic.volume = volume;
 backgroundMusic.loop = loop;
-backgroundMusic.play()
+
+const savedTime = localStorage.getItem('audioTime');
+if (savedTime) {
+  backgroundMusic.currentTime = parseFloat(savedTime);
+}
+backgroundMusic.play().catch(error => {
+  console.log("Autoplay failed:", error); 
+});
+backgroundMusic.addEventListener('timeupdate', () => {
+  localStorage.setItem('audioTime', backgroundMusic.currentTime);
+});
+window.addEventListener('beforeunload', () => {
+  localStorage.setItem('audioTime', backgroundMusic.currentTime);
+});
